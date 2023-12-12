2801c0b22b3ec0b649dfb23f69ddd17e60a96d72
Lucas dias <lucas.dias@fps.edu.br>
2023-12-12 10:40:45 -0300

    exemplo de construção de api

---
+++ goapi/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	routes "goapi/api/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	app := gin.Default()
+
+	routes.AppRoutes(app)
+
+	app.Run("localhost:3001")
+}
